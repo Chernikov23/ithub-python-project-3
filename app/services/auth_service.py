670d8eec2dc736d55905b8b31d4ccc1a088366e1
Gavrilov_Maksim <gavrilovmax1007@gmail.com>
2026-05-28 00:11:54 +0300

    done

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -5,14 +5,6 @@ from app.security import create_access_token, get_password_hash, verify_password
 
 
 def register(*, cursor: Cursor, user_data: schema.UserCreate) -> None:
-	"""
-	:cursor: курсор подключения к базе данных
-	:user_data: данные для регистрации
-
-	Добавляет пользователя в базу данных (хешируя пароль)
-	"""
-
-	# insert user with hashed password
 	hashed = get_password_hash(user_data.password)
 	cursor.execute(
 		"INSERT INTO users (username, password) VALUES (?, ?)",
@@ -21,13 +13,6 @@ def register(*, cursor: Cursor, user_data: schema.UserCreate) -> None:
 
 
 def authenticate(*, cursor: Cursor, user_data: schema.UserCreate) -> str | None:
-	"""
-	Находит пользователя по юзернейму,
-	сверяет хеш переданного пароля с истинным.
-	В случае несовпадения возвращает None.
-	Иначе - создает и возвращает токен доступа.
-	"""
-
 	cursor.execute('SELECT password FROM users WHERE username = ?', (user_data.username,))
 	row = cursor.fetchone()
 	if not row:
