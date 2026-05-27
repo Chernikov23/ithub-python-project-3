fbff3b103e065fd1c8e9eb99996dc0d669d46794
Gavrilov_Maksim <gavrilovmax1007@gmail.com>
2026-05-28 00:15:05 +0300

    done

--- app/schema.py
+++ app/schema.py
@@ -6,64 +6,29 @@ from pydantic import ConfigDict, Field, field_serializer
 
 
 class BaseModel(PBaseModel):
-	"""
-	Расширенная базовая модель, позволяет
-	красивее обрабатывать данные ORM-слоя
-	(правда отдельным методом, который вам
-	придётся разузнать)
-	"""
-
+	
 	model_config = ConfigDict(populate_by_name=True, from_attributes=True)
 
 
 class UserToken(BaseModel):
-	"""
-	Схема данных для oauth2-токенов, не требует изменений
-	"""
-
 	access_token: str
 	token_type: Literal['bearer'] = 'bearer'
 
 
 class UserCreate(BaseModel):
-	"""
-	TODO
-	Схема данных для создания пользователя
-
-	:username: строка длиной не менее 2 символов
-	:password: строка длиной не менее 8 символов
-	"""
-
 	username: str = Field(..., min_length=2)
 	password: str = Field(..., min_length=8)
 
 	@field_serializer('username')
-	def _strip_username(self, v: str) -> str:  # ensure no surrounding spaces
+	def _strip_username(self, v: str) -> str:  
 		return v.strip()
 
 
 class UserProfile(BaseModel):
-	"""
-	TODO
-	Схема данных для публичной информации пользователя
-
-	:username: строка
-	"""
-
 	username: str
 
 
 class Dream(BaseModel):
-	"""
-	TODO
-	Схема для выдачи данных сна
-
-	:id: целое число
-	:description: строка описания
-	:author: строка, юзернейм автора
-	:created_at: строка, дататайм формата ISO
-	"""
-
 	id: int
 	description: str
 	author: str
