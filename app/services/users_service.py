1a65048f83858f0ce84e561390b77e537d2b215d
Gavrilov_Maksim <gavrilovmax1007@gmail.com>
2026-05-28 00:14:15 +0300

    done

--- app/services/users_service.py
+++ app/services/users_service.py
@@ -6,14 +6,6 @@ from app.database import models
 
 
 def get_by_username(*, cursor: Cursor, username: str) -> schema.UserProfile | None:
-	"""
-	:cursor: курсор подключения к базе данных
-	:username: уникальный юзернейм пользователя
-
-	Запрашивает пользователя из базы данных.
-	Если пользователь не найден, возвращает None.
-	Иначе - возвращает запись согласно схеме.
-	"""
 	cursor.execute('SELECT username FROM users WHERE username = ?', (username,))
 	row = cursor.fetchone()
 	if not row:
